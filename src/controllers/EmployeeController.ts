768690164bd4f5c1f0a4537000f732156ff3cef2
Lucas Fernandes <luhckaspessoal@gmail.com>
2024-11-24 23:03:51 -0400

    feat: Cria um estado para o termo de pesquisa no Controller

--- src/controllers/EmployeeController.ts
+++ src/controllers/EmployeeController.ts
@@ -6,12 +6,15 @@ export const useEmployees = () => {
   const [employees, setEmployees] = useState<Employee[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredEmployees, setFilteredEmployees] = useState<Employee[]>([]);
 
   useEffect(() => {
     const getEmployees = async () => {
       try {
         const data = await fetchEmployees();
         setEmployees(data);
+        setFilteredEmployees(data);
       } catch (error) {
         setError(`Erro ao carregar os dados. ${error}`);
       } finally {
@@ -22,5 +25,22 @@ export const useEmployees = () => {
     getEmployees();
   }, []);
 
-  return { employees, loading, error };
+  useEffect(() => {
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    const filtered = employees.filter(
+      (employee) =>
+        employee.name.toLowerCase().includes(lowerCaseSearchTerm) ||
+        employee.job.toLowerCase().includes(lowerCaseSearchTerm) ||
+        employee.phone.includes(lowerCaseSearchTerm)
+    );
+    setFilteredEmployees(filtered);
+  }, [searchTerm, employees]);
+
+  return {
+    employees: filteredEmployees,
+    loading,
+    error,
+    searchTerm,
+    setSearchTerm,
+  };
 };
