5355796bebb50da20735a4232a4a94b3e0ceaba6
Lucas Fernandes <luhckaspessoal@gmail.com>
2024-11-24 23:40:07 -0400

    feat: Adiciona um input de busca ao componente App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEmployees } from './controllers/EmployeeController';
 import { EmployeeTable } from './components/EmployeeTable';
 
 const App: React.FC = () => {
-  const { employees, loading, error } = useEmployees();
+  const { employees, loading, error, searchTerm, setSearchTerm } = useEmployees();
 
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>{error}</p>;
@@ -11,6 +11,12 @@ const App: React.FC = () => {
   return (
     <div>
       <h1>Funcionários</h1>
+      <input
+        type="text"
+        placeholder="Pesquisar por nome, cargo ou telefone"
+        value={searchTerm}
+        onChange={({target}) => setSearchTerm(target.value)}
+      />
       <EmployeeTable employees={employees} />
     </div>
   );
