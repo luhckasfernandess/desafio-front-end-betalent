5408ba750b2e1cddcc298bba72c6ace0d45d7ee7
Lucas Fernandes <luhckaspessoal@gmail.com>
2024-11-26 20:29:36 -0400

    style: Adiciona estilo para o cabeçalho da página

--- src/components/HeaderTable.tsx
+++ src/components/HeaderTable.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/Header.css';
 
 export const HeaderTable: React.FC = () => {
   const logo = "../src/assets/betalent-logo.svg";
 
   return (
     <header>
-      <img src={logo} alt="Logo Betalent" />
+      <img className='logo' src={logo} alt="Logo Betalent" />
     </header>
   );
 };
